590994854a46ed0e41bd14812b3eb6da93f98431
Luthando-D <62010426+Luthando-D@users.noreply.github.com>
2023-08-02 10:25:18 +0200

    my commit

---
+++ JS/about.js
@@ -0,0 +1,17 @@
+function myToggle() {
+    var x = document.getElementById("cardBody1");
+    if (x.style.display === "none"){
+        x.style.display = "block";
+    } else{
+        x.style.display = "none";
+    }
+}
+
+function myToggle2() {
+    var x = document.getElementById("cardBody2");
+    if (x.style.display === "none"){
+        x.style.display = "block";
+    } else{
+        x.style.display = "none";
+    }
+}
---
+++ JS/home.js
@@ -0,0 +1,1 @@
+const languageSelect = document.getElementById("languageSelect")
